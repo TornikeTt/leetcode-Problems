57f695370caa0d8c2ec3abd90610c87dccfe11ab
Tornike <chargeishvilitornike@gmail.com>
2024-06-29 18:35:32 -0700

    Solved N-1266 leetcode problem

--- Easy/0044. 1266-Minimum Time Visiting All Points/1266.js
+++ Easy/0044. 1266-Minimum Time Visiting All Points/1266.js
@@ -1,19 +1,22 @@
 var minTimeToVisitAllPoints = function(points) {
-    
     let count = 0;
     
     for ( let i = 0; i < points.length - 1; i++ ) { 
-        if( points[i][0] < points[i+1][0]) {
-            let hold = points[i+1][0]
-            points[i+1][0] = points[i][0]
-            points[i][0] = hold
-        } 
+        let first = points[i][0] - points[i+1][0];
+        let second = points[i][1] - points[i+1][1];
+        
+        let newFirst = Math.abs(first)
+        let newSecond = Math.abs(second)
 
-        console.log(points)
+        if( newFirst > newSecond ){ 
+            count+=newFirst
+        } else { 
+            count+=newSecond
+        }
     }
 
     return count
 };
 
 minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]]);
-//minTimeToVisitAllPoints([[3,2],[-2,2]]);
+minTimeToVisitAllPoints([[3,2],[-2,2]]);
