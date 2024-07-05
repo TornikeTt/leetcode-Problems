f08f47bafb092381c5dd8f9f89c5b331ed6871c3
Tornike <chargeishvilitornike@gmail.com>
2024-07-04 20:14:27 -0700

    Solved N-561

---
+++ Easy/0059. 561-Array Partition/561.js
@@ -0,0 +1,17 @@
+var arrayPairSum = function (nums) {
+    let correctNums = nums.sort((a, b) => a - b);
+
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) {
+            sum += nums[i];
+        }
+    }
+
+    return sum;
+};
+
+// console.log(arrayPairSum([1, 4, 3, 2]));
+
+console.log(arrayPairSum([6, 2, 6, 5, 1, 2]));
