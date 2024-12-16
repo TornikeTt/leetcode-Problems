9d502cbba40f12f46de55ee301a81e8b6ad9df12
Tornike <chargeishvilitornike@gmail.com>
2024-12-16 15:44:09 -0800

    solved N-11

---
+++ Medium/0016. 11. Container With Most Water/11.js
@@ -0,0 +1,20 @@
+var maxArea = function (height) {
+    let left = 0;
+    let right = height.length - 1;
+    let res = 0;
+
+    while (left < right) {
+        const area = Math.min(height[left], height[right]) * (right - left);
+        // 8 49 18 40 24 6 10 4
+        res = Math.max(res, area);
+        if (height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return res;
+};
+
+maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
