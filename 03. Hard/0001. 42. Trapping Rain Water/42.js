8583888133058d7ef2b8cc188fb2f438ebb9f2e6
Tornike <chargeishvilitornike@gmail.com>
2025-04-14 06:57:37 +0400

    solved N-42 problem

--- 03. Hard/0001. 42. Trapping Rain Water/42.js
+++ 03. Hard/0001. 42. Trapping Rain Water/42.js
@@ -1,3 +1,34 @@
-var trap = function (height) {};
+function trap(height) {
+
+    if (!height || height.length === 0) {
+        return 0;
+    }
+
+    let left_pointer = 0;
+    let right_pointer = height.length - 1;
+
+    let leftMax = height[left_pointer]; 
+    let rightMax = height[right_pointer];
+
+    let res = 0;
+
+    while (left_pointer < right_pointer) {
+
+        if (leftMax < rightMax) {
+            left_pointer++;
+
+            leftMax = Math.max(leftMax, height[left_pointer]);
+            res += leftMax - height[left_pointer];
+        } else {
+            right_pointer--;
+
+            rightMax = Math.max(rightMax, height[right_pointer]);
+            res += rightMax - height[right_pointer];
+        }
+
+    }
+
+    return res;
+}
 
 trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]);
---
+++ 03. Hard/0001. 42. Trapping Rain Water/README.md
@@ -0,0 +1,14 @@
+# Trapping Rain Water
+
+## 🧠 Problem Statement
+
+Given `n` non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+## 🧪 Examples
+
+### Example 1
+
+``` javascript
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+```
